64d45375bc41ab22ad1d01ea89e68126d32fe2ef
Daniel Kuffner <daniel.kuffner@avid.com>
2018-03-19 18:33:27 +0100

    fix version

--- azure.go
+++ azure.go
@@ -54,7 +54,7 @@ type client struct {
 }
 
 const serviceBusURL = "https://%s.servicebus.windows.net:443/"
-const apiVersion = "2017-04-01"
+const apiVersion = "2017-04"
 
 //New creates a new client from the given parameters. Their meaning can be found in the MSDN docs at:
 //  https://docs.microsoft.com/en-us/rest/api/servicebus/Introduction
